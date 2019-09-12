9e59a7e3a71046540c5cb9ebb959d5253ae45564
Stanislav <stas.starodubtsev@gmail.com>
2019-09-12 10:49:39 +0300

    Task3_LoginScenario

---
+++ Task_3-LoginScenario/LoginScenario/LoginScenario/LogScenario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+
+namespace LoginScenario
+{
+    [TestFixture]
+    public class LogScenario
+    {
+        private IWebDriver driver;
+        private readonly string _baseUrl = "http://localhost:8080/litecart/admin/";
+
+        [SetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+        }
+
+        [Test]
+        public void Login()
+        {
+            driver.Url = _baseUrl;
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+            driver.FindElement(By.Name("login")).Click();
+            string currentUrl = driver.Url;
+            Assert.AreEqual(_baseUrl, currentUrl);
+        }
+
+        [TearDown]
+        public void Stop()
+        {
+            Thread.Sleep(2000);
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
