45d2e80128ca4869afb859ed3f6f5a0fee45ddff
Stanislav <stas.starodubtsev@gmail.com>
2019-09-18 18:40:52 +0300

    Task8

---
+++ Task8_StikerScenario/Task8_StikerScenario/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task8_StikerScenario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task8_StikerScenario")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("542f34dd-d3aa-4911-b5c5-f502c9742759")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task8_StikerScenario/Task8_StikerScenario/StikerScenario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+
+namespace Task8_StikerScenario
+{
+    [TestFixture]
+    public class StikerScenario
+    {
+        private IWebDriver driver;
+        private readonly string _baseUrl = "http://localhost:8080/litecart/";
+
+        private const string _mostPopularSection = "//*[@id='box-most-popular']//li";
+        private const string _campaignSection = "//*[@id='box-campaigns']//li";
+        private const string _latestProductsSection = "//*[@id='box-latest-products']//li";
+
+        [OneTimeSetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            driver.Url = _baseUrl;
+        }
+
+        [TestCase(_mostPopularSection, TestName = "Most Popular section")]
+        [TestCase(_campaignSection, TestName = "Campaigns section")]
+        [TestCase(_latestProductsSection, TestName = "Latest products section")]
+        public void SectionTest(string section)
+        {
+            IList<IWebElement> elements = driver.FindElements(By.XPath(section));
+
+            if (elements.Count != 0)
+            {
+                for (int i = 1; i <= elements.Count; i++)
+                {
+                    Assert.AreEqual(1, driver.FindElements(By.XPath($"{section}[{i}]//div[contains(@class, 'sticker')]")).Count);
+                }
+            }
+        }
+
+        [OneTimeTearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
