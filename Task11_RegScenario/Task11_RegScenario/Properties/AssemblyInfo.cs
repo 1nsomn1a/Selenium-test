aaccb80b02df06c69414e0150ffae87f91e09f36
Stanislav <stas.starodubtsev@gmail.com>
2019-09-29 20:55:46 +0300

    Task11

---
+++ Task11_RegScenario/Task11_RegScenario/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Task11_RegScenario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task11_RegScenario")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e0b5bd68-29da-4027-8fd0-e4cb7aedc1cf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task11_RegScenario/Task11_RegScenario/Scenario.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Linq;
+
+namespace Task11_RegScenario
+{
+    [TestFixture]
+    public class Scenario
+    {
+        private IWebDriver driver;
+        private readonly string _MainPage = "http://localhost:8080/litecart/en/";
+
+        [OneTimeSetUp]
+        public void Start()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [Test]
+        public void SignUp()
+        {
+            driver.Url = _MainPage;
+            string email = EmailAddress();
+            string password = "password123";
+
+            driver.FindElement(By.XPath("//tbody//a")).Click();
+
+            driver.FindElement(By.XPath("//*[@name='firstname']")).SendKeys("Arnold");
+            driver.FindElement(By.XPath("//*[@name='lastname']")).SendKeys("Schwarzenegger");
+            driver.FindElement(By.XPath("//*[@name='address1']")).SendKeys("RandomStreet str.");
+            driver.FindElement(By.XPath("//*[@name='postcode']")).SendKeys("12345");
+            driver.FindElement(By.XPath("//*[@name='city']")).SendKeys("Thal");
+
+            driver.FindElement(By.XPath("//*[@class='select2 select2-container select2-container--default']")).Click();
+            driver.FindElement(By.XPath("//*[@class='select2-search__field']")).SendKeys("united states");
+            driver.FindElement(By.XPath("//*[@class='select2-results__options']/li[1]")).Click();
+
+
+            driver.FindElement(By.XPath("//*[@name='email']")).SendKeys(email);
+            driver.FindElement(By.XPath("//*[@name='phone']")).SendKeys("+235294583498");
+            driver.FindElement(By.XPath("//*[@name='password']")).SendKeys(password);
+            driver.FindElement(By.XPath("//*[@name='confirmed_password']")).SendKeys(password);
+            driver.FindElement(By.XPath("//*[@name='create_account']")).Click();
+
+            driver.FindElement(By.XPath("//*[@id='box-account']//ul/li[4]/a")).Click();
+
+            driver.FindElement(By.XPath("//*[@name='email']")).SendKeys(email);
+            driver.FindElement(By.XPath("//*[@name='password']")).SendKeys(password);
+            driver.FindElement(By.XPath("//*[@name='login']")).Click();
+
+            driver.FindElement(By.XPath("//*[@id='box-account']//ul/li[4]/a")).Click();
+        }
+
+        [OneTimeTearDown]
+        public void Stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+        public string EmailAddress()
+        {
+            var characters = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789";
+            var random = new Random();
+            var EmailAddress = new string(Enumerable.Repeat(characters, 10).Select(s => s[random.Next(s.Length)]).ToArray());
+            EmailAddress = EmailAddress + "@test123.com";
+            return EmailAddress;
+        }
+    }
+}
